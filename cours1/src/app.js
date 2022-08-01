018a3f5460afe8b739c0051dd3979bce795045d6
qfdk <qfdk2010@gmail.com>
2022-08-01 15:58:06 +0200

    fix: cours 1

---
+++ cours1/src/app.js
@@ -0,0 +1,16 @@
+const CONSTANT_VAR = [1, "我是字符串", true];
+type1 = "我是字符串";
+console.log(typeof type1)
+
+type1 = 1;
+console.log(typeof type1)
+
+type1 = true;
+console.log(typeof type1)
+
+type1 = [1, "我是字符串", true];
+console.log(typeof type1);
+
+// console.log("你好1");
+// console.log(type1);
+// console.log("你好2");
